2e1bf3f362b1c89ce28719ad552bdad73a73bb82
Jinsol Kim <jinsol032360@gmail.com>
2024-03-06 10:15:27 +0900

    add : props 응용하여 모달창 제목 변경

--- src/App.js
+++ src/App.js
@@ -1,103 +1,123 @@
 /*eslint-disable*/
 
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  let [title, setTitle] = useState([
-    "남자코트 추천",
-    "강남 우동맛집",
-    "파이썬독학",
-  ]);
-  let post = "강남 우동 맛집";
-  let [like, setLike] = useState([0, 0, 0]);
-  let [modal, setModal] = useState(false);
+    let [title, setTitle] = useState(['남자코트 추천', '강남 우동맛집', '파이썬독학']);
+    let [like, setLike] = useState([0, 0, 0]);
+    let [modal, setModal] = useState(false);
+    let [modalTitle, setModalTitle] = useState(0);
 
-  return (
-    <div className="App">
-      <div className="black-nav">
-        <h4>blog</h4>
-      </div>
+    return (
+        <div className="App">
+            <div className="black-nav">
+                <h4>reactBlog</h4>
+            </div>
 
-      <button
-        className="button"
-        onClick={() => {
-          let copy = [...title]; // spread operator : 괄호 벗기기용 연산자, array나 object 자료형을 복사할 때 많이 사용함
-          copy = ["여자코트 추천", "강남 우동맛집", "파이썬독학"];
-          // console.log(copy);
-          setTitle(copy);
-        }}
-      >
-        변해라얍
-      </button>
-      <button
-        className="button_sort"
-        onClick={() => {
-          let copy = [...title];
-          copy.sort();
-          setTitle(copy);
-          // console.log(copy);
-        }}
-      >
-        정렬버튼
-      </button>
-
-      {title.map(function (a, i) {
-        return (
-          <div className="list" key={i}>
-            <h4>
-              <span
-                style={{ cursor: "pointer" }}
+            <button
+                className="button"
                 onClick={() => {
-                  setModal(!modal);
+                    // spread operator : 괄호 벗기기용 연산자, array나 object 자료형을 복사할 때 많이 사용함
+                    let copy = [...title]; 
+                    copy = ['여자코트 추천', '강남 우동맛집', '파이썬독학'];
+                    // console.log(copy);
+                    setTitle(copy);
                 }}
-              >
-                {title[i]}
-              </span>
-              <span
-                style={{ cursor: "pointer" }}
+            >
+                변해라얍
+            </button>
+            <button
+                className="button_sort"
                 onClick={() => {
-                  let copy = [...like];
-                  // console.log(copy);
-                  copy[i] = copy[i] + 1;
-                  // console.log(copy[i]);
-                  setLike(copy);
+                    let copy = [...title];
+                    copy.sort();
+                    setTitle(copy);
+                    // console.log(copy);
                 }}
-              >
-                👍
-              </span>{" "}
-              {like[i]}{" "}
-            </h4>
+            >
+                정렬버튼
+            </button>
 
-            <p>3월 6일 발행</p>
-          </div>
-        );
-      })}
+            {title.map(function (a, i) {   // map 안에서의 i는 반복문이 돌 때마다 0,1,2... 이렇게 증가하는 정수다. 
+                return (
+                    <div className="list" key={i}>
+                        <h4>
+                            <span
+                                style={{ cursor: 'pointer' }}
+                                onClick={() => {
+                                    setModal(!modal);
+                                    setModalTitle(i);
+                                }}
+                            >
+                                {title[i]}
+                            </span>
+                            <span
+                                style={{ cursor: 'pointer' }}
+                                onClick={() => {
+                                    let copy = [...like];
+                                    // console.log(copy);
+                                    copy[i] = copy[i] + 1;
+                                    // console.log(copy[i]);
+                                    setLike(copy);
+                                }}
+                            >
+                                👍
+                            </span>{' '}
+                            {like[i]}{' '}
+                        </h4>
 
-      {modal === true ? <Modal title={title} setTitle={setTitle} /> : null}
-    </div>
-  );
+                        <p>3월 6일 발행</p>
+                    </div>
+                );
+            })}
+
+            <button
+                onClick={() => {
+                    setModalTitle(0);
+                }}
+            >
+                0번글
+            </button>
+            <button
+                onClick={() => {
+                    setModalTitle(1);
+                }}
+            >
+                1번글
+            </button>
+            <button
+                onClick={() => {
+                    setModalTitle(2);
+                }}
+            >
+                2번글
+            </button>
+
+            {modal === true ? <Modal modalTitle={modalTitle} title={title} setTitle={setTitle} /> : null}
+        </div>
+    );
 }
 
 function Modal(props) {
-  return (
-    <div className="modal">
-      <h4>{props.title[0]}</h4>
-      <p>날짜</p>
-      <p>상세내용</p>
-      <button
-        onClick={() => {
-          let copy = [...props.title];
-          console.log(copy);
-          copy = ["여자코트 추천", "강남 우동맛집", "파이썬독학"];
-          console.log(copy);
-          props.setTitle(copy);
-        }}
-      >
-        글수정
-      </button>
-    </div>
-  );
+    return (
+        <div className="modal">
+            <h4>{props.title[props.modalTitle]}</h4>
+            <p>날짜</p>
+            <p>상세내용</p>
+            <button
+                onClick={() => {
+                    let modalCopy = [...props.title];
+                    // console.log(modalCopy);
+                    modalCopy = ['여자코트 추천', '강남 우동맛집', '파이썬독학'];
+                    // console.log(modalCopy);
+                    props.setTitle(modalCopy);
+                }}
+            >
+                글수정
+            </button>
+        </div>
+    );
 }
 
 export default App;
